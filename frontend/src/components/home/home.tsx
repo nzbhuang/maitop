ad1865e3eee829d13ca3e5b1d6bdf5fd9ff721c9
brian huang <nz.brianhuang@gmail.com>
2024-07-29 03:22:10 +1200

    add simple home page

--- frontend/src/components/home/home.tsx
+++ frontend/src/components/home/home.tsx
@@ -1,7 +1,22 @@
+import { Stack, Image, Box } from '@mantine/core';
+import Whahuh from '../../assets/whahuh.jpg'
+import classes from './home.module.css'
 
 const Home = () => {
     return (
-        <p>Home</p>
+        <Box className={classes.page}>
+            <Stack align="center" justify="center" gap={0}>
+                <h1 className={classes.title}>maitop</h1>
+                <p className={classes.description}>Simple tracker of your top scores from maimai!</p>
+                <Box style={{ height: '20px'}}/>
+                <Image
+                    className={classes.image}
+                    src={Whahuh}
+                    radius="sm" 
+                />
+                <p className={classes.credit}>art drawn by friend and used with permission</p>
+            </Stack>
+        </Box> 
     )
 }
 
