784f8f9ad8c61c4072f62b09e0bc8642b6ebc767
brian huang <nz.brianhuang@gmail.com>
2024-08-03 23:33:00 +1200

    fix accuracy to double

--- backend/Models/Score.cs
+++ backend/Models/Score.cs
@@ -4,7 +4,7 @@
     {
         public int ScoreId { get; set; }
         public int ChartId { get; set; }
-        public float Accuracy { get; set; }
+        public double Accuracy { get; set; }
         public int ScoreRating { get; set; }
         
         public required Chart Chart { get; set; }
